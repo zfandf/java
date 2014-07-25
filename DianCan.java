e58ae78c833a9db70e9802b606a10849eb1ca683
zhangff <zhangff@m15.cn>
2014-07-25 18:21:10 +0800

    Update

--- DianCan.java
+++ DianCan.java
@@ -6,7 +6,7 @@ public class DianCan {
     public static void main(String [] args) {
 
         //String[] canList = {"market","malan","xiaodou","kku","huoshao", "xiapu", "huimian"};
-        String[] canList = {"market","malan","xiaodou","kku","huoshao", "xiapu", "huimian"};
+        String[] canList = {"kku", "xiapu", "huimian"};
 
         int catNum = canList.length;
         int num = (int)(Math.random()*catNum);
